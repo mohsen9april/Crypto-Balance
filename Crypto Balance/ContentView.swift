5d7b707384ba9e3ee74de151dc6a469694034339
Mohsen Abdollahi <mohsen9april@gmail.com>
2020-08-28 18:02:43 +0430

    Initial Commit

---
+++ Crypto Balance/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Crypto Balance
+//
+//  Created by Mohsen Abdollahi on 8/28/20.
+//  Copyright © 2020 Mohsen Abdollahi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
