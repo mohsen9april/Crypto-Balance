3f927320d579196bff10939ec66f5d61e3c6a5f1
Mohsen Abdollahi <mohsen9april@gmail.com>
2020-08-30 15:36:45 +0430

    Make a List with section

--- Crypto Balance/ContentView.swift
+++ Crypto Balance/ContentView.swift
@@ -9,8 +9,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var rates: [Coin] = [
+        Coin(id: "BTC", name: "Bitcoin", price: "9733.95", icon: "bitcoin"),
+        Coin(id: "LTC", name: "Litecoin", price: "78.70", icon: "litecoin"),
+        Coin(id: "XIP", name: "Ripple", price: "0.30", icon: "ripple"),
+        Coin(id: "TRX", name: "Tron", price: "0.02", icon: "tron"),
+        Coin(id: "ETH", name: "Ethreum", price: "200.45", icon: "ethereum")
+    ]
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        NavigationView{
+            
+            VStack{
+                Text("your Crypto Balance")
+                Text("$ 3.133.7").font(.largeTitle).fontWeight(.heavy)
+                
+                List {
+                    
+                    Section(header: Text("Current Prices")) {
+                        ForEach(rates){ item in
+                            HStack {
+                                Text("\(item.id)")
+                                Text("\(item.name)")
+                            }
+                        }
+                        
+                    }
+                }.listStyle(GroupedListStyle())
+            }
+            .navigationBarTitle(Text("Dashboard"))
+            .navigationBarHidden(true)
+        }
     }
 }
 
@@ -18,4 +49,12 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Coin : Identifiable {
+    
+    let id: String
+    let name: String
+    let price: String
+    let icon: String
 }
